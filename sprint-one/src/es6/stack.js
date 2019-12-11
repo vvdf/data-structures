4ebd69b1bc078a45c0f182c72127a8dc5dae086a
7FFF00 <t42please@gmail.com>
2019-12-11 15:24:39 -0800

    Implement stack using es6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,21 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+      var newestIndex = this.size().toString();
+      this.storage[newestIndex] = value;
+  };
+
+  pop() {
+      var poppedElement = this.storage[(this.size() - 1).toString()];
+      delete this.storage[(this.size() - 1).toString()];
+      return poppedElement;
+  };
+
+  size() {
+      return (Object.values(this.storage).length);
+  };
 }
